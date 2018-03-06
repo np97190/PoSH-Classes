e58a1609f6bf979b8af574e51cfcd1683a07fdd5
np97190 <gpk.pandey.neeraj@gmail.com>
2018-03-06 09:02:59 +0530

    Create Cusomobje_Ex1.ps1

---
+++ Cusomobje_Ex1.ps1
@@ -0,0 +1,42 @@
+try
+{
+   $ErrorActionPreference = "stop"
+    #Memory Usages
+   $OS = Get-WmiObject -Class win32_operatingsys
+   $TotalMemorySize = $OS.TotalVisibleMemorySize
+   $freeMemorySize = $OS.FreePhysicalMemory
+   
+   #Cdrive uses
+   $Disk = Get-WmiObject -Class win32_logicaldisk -Filter "DeviceID='c:'"
+   $DIskSize      = $Disk.Size
+   $freeDiskSpace = $Disk.FreeSpace
+
+   #CPU Load
+   $Cpu = Get-WmiObject -Class win32_processor
+   $load = $Cpu.LoadPercentage
+
+   #OS Arch
+   $arch = $OS.OSArchitecture
+
+   $result = [PSCustomObject]@{
+   
+        Host = $env:COMPUTERNAME
+        MemoryUsages = [Math]::Round((($TotalMemorySize - $freeMemorySize)/1mb),2)
+        OSDriveUsages = [Math]::Round(($freeDiskSpace /1GB),2)
+        OSArchitecture = $arch
+   }
+}
+catch
+{
+    $result = [PSCustomObject]@{
+   
+        Host = $env:COMPUTERNAME
+        MemoryUsages = "Undefined"
+        OSDriveUsages = "Undefined"
+        OSArchitecture = "Undefined"
+   }
+}
+finally
+{
+    Write-Output $result
+}
